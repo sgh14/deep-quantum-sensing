b01e025aa96102d2a60640d306a2ddca189c55f4
Sergio García Heredia <sergio.gh14@gmail.com>
2023-05-26 10:48:39 +0200

    Add `FrecuencyLayer`

--- models.py
+++ models.py
@@ -1,5 +1,22 @@
+import tensorflow as tf
 from tensorflow.keras import layers, models
 
+
+class FrequencyLayer(layers.Layer):
+    def __init__(self, spin, **kwargs):
+        super(FrequencyLayer, self).__init__(**kwargs)
+        self.spin_vals = list(range(-spin, spin + 1))
+
+
+    def call(self, inputs):
+        counts = []
+        for s in self.spin_vals:
+            counts.append(tf.reduce_sum(tf.cast(tf.equal(inputs, s), tf.int32), axis=1))
+
+        frequencies = tf.divide(tf.stack(counts, axis=1), inputs.shape[1])
+
+        return frequencies
+    
 
 def get_plain_model(input_shape, output_shape, config):
     nmeasures = input_shape[0]
@@ -21,7 +38,12 @@ def get_plain_model(input_shape, output_shape, config):
 def get_conv_model(input_shape, output_shape, config):
     dropout = config['dropout']
     model = models.Sequential()
-    model.add(layers.Reshape(input_shape + (1,), input_shape=input_shape))
+    if config['layers']['frequency_layer']:
+        spin = config['layers']['frequency_layer']
+        model.add(FrequencyLayer(spin=spin, input_shape=input_shape))
+        input_shape = (2*spin + 1, input_shape[1])
+    
+    model.add(layers.Reshape(input_shape + (1,), input_shape=input_shape)) # (batch, vals, qudits, 1))
     for layer_config in config['layers']['conv_layers']:
         model.add(layers.Conv2D(**layer_config))
         if dropout:
@@ -48,7 +70,12 @@ def get_recurrent_model(input_shape, output_shape, config):
         recurrent_layer = layers.SimpleRNN
 
     model = models.Sequential()
-    model.add(layers.Permute((2, 1), input_shape=input_shape)) # (batch, qubits, measures))
+    if config['layers']['frequency_layer']:
+        spin = config['layers']['frequency_layer']
+        model.add(FrequencyLayer(spin=spin, input_shape=input_shape))
+        input_shape = (2*spin + 1, input_shape[1])
+
+    model.add(layers.Permute((2, 1), input_shape=input_shape)) # (batch, qudits, vals))
     for layer_config in config['layers']['recurrent_layers']:
         model.add(layers.Bidirectional(recurrent_layer(**layer_config)))
         if dropout:
